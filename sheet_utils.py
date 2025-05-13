ce4f0f3ea0adf8650e5cf3cb049f74967ff82b1b
ChaoLiangChen23 <slowsoul0323@gmail.com>
2025-05-14 01:07:56 +0800

    Update sheet_utils.py

--- sheet_utils.py
+++ sheet_utils.py
@@ -3,11 +3,12 @@ from google.oauth2.service_account import Credentials
 import os
 
 def write_to_sheet(data: list):
-    creds = Credentials.from_service_account_file("webhook-gsheet-0cddc63f4d59.json", scopes=[
+    creds_path = os.getenv("GSHEET_CREDENTIALS")  # ✅ 從環境變數取得 JSON 金鑰檔名
+    creds = Credentials.from_service_account_file(creds_path, scopes=[
         "https://www.googleapis.com/auth/spreadsheets"
     ])
     client = gspread.authorize(creds)
-    
-    sheet_url = os.getenv("SHEET_URL")
+
+    sheet_url = os.getenv("SHEET_URL")  # ✅ 從環境變數取得 Google Sheet URL
     sheet = client.open_by_url(sheet_url).sheet1
     sheet.append_row(data, value_input_option="USER_ENTERED")
